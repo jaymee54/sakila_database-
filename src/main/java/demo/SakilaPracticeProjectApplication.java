8ff0fb01c99822ea7e1a1b20397ff7a79a62f192
jaymee54 <Jamielomas54@yahoo.co.uk>
2022-10-20 11:56:00 +0100

    skila database project basis

---
+++ src/main/java/demo/SakilaPracticeProjectApplication.java
@@ -0,0 +1,13 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SakilaPracticeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SakilaPracticeProjectApplication.class, args);
+	}
+
+}
